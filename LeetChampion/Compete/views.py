0ebc740379bd6f2263e8c79ed90aacaed46d4e53
ejDeister <ethandeister@gmail.com>
2024-10-11 16:28:13 -0700

    Added a 'get-problem' view and more meaningful comments to compete/views.py

--- LeetChampion/Compete/views.py
+++ LeetChampion/Compete/views.py
@@ -2,30 +2,34 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseNotFound
 from . import forms
 
-# Define a default view
+# Define a default view (header, navbar, footer etc.) that content will be injected into
 def index(request):
     return render(request, 'index.html')
 
-# Define a default view
+# Define a home view
 def home(request):
     return render(request, 'home.html')
 
-# Define a default view
+# Define a dashboard view
 def dashboard(request):
     return render(request, 'dashboard.html')
 
-# Define a default view
+# Define a view to either conenct a user to a lobby or host their own
 def lobby(request):
     return render(request, 'lobby.html')
 
-# Define a default view
+# Define a view to get an LC problem
+def get_poblem(request):
+    return render(request, 'get-problem.html')
+
+# Define a view to run a solution to an LC problem
 def run(request):
     return render(request, 'run-solution.html')
 
-# Define a default view
+# Define a view to submit a solution to an LC problem
 def submit(request):
     return render(request, 'submit-solution.html')
 
-# Define a default view
+# Define a view to display results of a battle
 def results(request):
     return render(request, 'results.html')
