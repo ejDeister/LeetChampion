71a0075eade7fcf926d08eda2bc431d21d5872b5
ejDeister <ethandeister@gmail.com>
2024-10-12 17:22:12 -0700

    Added project URLs for all apps, updated settings to install all apps and to update the static files directory.

--- LeetChampion/LeetChampion/urls.py
+++ LeetChampion/LeetChampion/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('compete/', include('Compete.urls')),
+    path('auth,', include('Authentication.urls')),
+    path('host/', include('Host.urls')),
+    path('tournament', include('Tournament.urls')),
 ]
