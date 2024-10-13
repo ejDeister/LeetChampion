d3cf76df1682e737db33f06978277468a53bf685
ejDeister <ethandeister@gmail.com>
2024-10-12 17:47:34 -0700

    Added basic lobby structure for testing LC API. No implementation for the API just yet. Added a TODO for the secret key. A new key will be generated and stored in .env, which will be ignored by git. The .env file will be stored in a private github repo to be shared between collaborators/devs.

--- LeetChampion/LeetChampion/settings.py
+++ LeetChampion/LeetChampion/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+#TODO: SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-m6oycpc4d&zbnwlq+jlrj2$e(9l^zy=*42eu+h!$t81(=12hx&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
